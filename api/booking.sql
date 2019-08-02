cd24a2af000da7508803b29a8525663e78c592cd
Adam Snodgrass <overra@gmail.com>
2019-08-01 22:21:36 -0500

    add booking.sql

---
+++ api/booking.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `booking` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `address` VARCHAR(45) NOT NULL,
+  `city` VARCHAR(45) NOT NULL,
+  `state` VARCHAR(45) NOT NULL,
+  `zipcode` VARCHAR(45) NOT NULL,
+  `type` ENUM('HOUSEKEEPING', 'DOG_WALK') NOT NULL,
+  `date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`));
